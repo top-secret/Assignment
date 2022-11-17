869b521f3c28007d8b9e1b3a1a07f634b3409064
Himanshu Gupta <himanshu30.dce@gmail.com>
2022-11-17 13:48:32 +0530

    Added Assignment 2 Statement

---
+++ Assignment_2/src/counter.ts
@@ -0,0 +1,16 @@
+export function counter(num = 0) {
+
+
+}
+
+// const [getA, nextA] = counter(1);
+// getA(); // 1
+// nextA();
+// getA(); // 2
+// const [getB, nextB] = counter(10);
+// nextB();
+// getA(); // 2
+// getB(); // 11
+// nextA();
+// getA(); // 3
+// getB(); // 11
