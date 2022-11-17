ef8ff89bb4698485ae9f88055047755f056f2c2f
Himanshu Gupta <himanshu30.dce@gmail.com>
2022-11-17 15:47:20 +0530

    Refractored and Cleaned Assignment 1

--- Assignment_1/src/index.ts
+++ Assignment_1/src/index.ts
@@ -1,5 +1,7 @@
 import * as https from "https";
 
+/********** DEFINING CONSTANTS, TYPES, ENUMS, INTERFACES *******/
+
 enum RequestMethod {
   get = "GET",
   post = "POST",
@@ -8,7 +10,57 @@ enum RequestMethod {
   delete = "DELETE",
 }
 
-interface RequestOptions {
+/**
+ * Type declaration for Episode Entity
+ */
+ export interface Episode {
+  id: number;
+  name: string;
+  air_date: string;
+  episode: string;
+  characters: string[];
+  url: string;
+  created: string; 
+}
+
+/**
+* Type declaration for Episode Character Entity
+*/
+export interface EpisodeCharacter {
+  id: number;
+  name: string;
+  status: string;
+  species: string;
+  type?: string;
+  gender: string;
+  origin: EpisodeCharacterOrigin;
+  location: EpisodeCharacterLocation;
+  episode: string[];
+  image: string;
+  url: string;
+  created: string;
+}
+
+/**
+* Type declaration for Episode Character origin Entity
+*/
+interface EpisodeCharacterOrigin {
+  name: string;
+  url: string;
+}
+
+/**
+* Type declaration for Episode Character Location Entity
+*/
+interface EpisodeCharacterLocation {
+  name: string;
+  url: string;
+}
+
+/**
+* Type declaration for Http request
+*/
+export interface HttpRequestOptions {
   hostname: string;
   path?: string,
   method: RequestMethod,
@@ -17,89 +69,160 @@ interface RequestOptions {
   }
 }
 
-const episodeRequestOptions: RequestOptions = {
-  hostname: 'rickandmortyapi.com',
-  path: '/api/episode',
+/**
+* Type declaration for HTTP Response
+*/
+export interface HttpResponse<T = any> {
+  data?: T;
+  success?: boolean;
+  error?: boolean;
+}
+
+/**
+* Type declaration defining type of Episode API response
+*/
+export interface EpisodeHttpReponse {
+  results: Episode[]
+}
+
+
+const episodeEndpointHttpRequestOptions: HttpRequestOptions = {
+  hostname: "rickandmortyapi.com",
+  path: "/api/episode",
   method: RequestMethod.get,
   headers: {
-    'Content-Type': 'application/json',
+    "Content-Type": "application/json",
   },
 };
 
-const characterRequestOptions: RequestOptions = {
-  hostname: 'rickandmortyapi.com',
+const characterEndpointHttpRequestOptions: HttpRequestOptions = {
+  hostname: "rickandmortyapi.com",
   method: RequestMethod.get,
   headers: {
-    'Content-Type': 'application/json',
+    "Content-Type": "application/json",
   },
 };
 
-const hitExternalRequest = async (url: string) => {
-  let data = '';
+/********** MAIN CODE FOR THE ASSIGNMENT *******/
 
-  const finalResponse = new Promise((res, rej) => {
-    const request = https.get(url, (response) => {
-      // Avoding binary data
-      response.setEncoding('utf8');
-  
+/**
+ * Common helper to make external HTTP request
+ *
+ * @param {HttpRequestOptions} options
+ * @returns {Promise<HttpResponse<T>>}
+ */
+const makeExternalRequest = async <T>(
+  options: HttpRequestOptions
+): Promise<HttpResponse<T>> => {
+  return new Promise((resolve, reject) => {
+    let data = "";
+
+    const request = https.request(options, (response) => {
+      // Avoiding binary data
+      response.setEncoding("utf8");
+
       // Concatenating Chunks into one variable while streaming
-      response.on('data', (chunk) => {
+      response.on("data", (chunk) => {
         data += chunk;
       });
-  
+
       // Listener which gets executed on receiving the complete response
-      response.on('end', () => {
-        let result: any = JSON.parse(data);
-        let resultObj = {
+      response.on("end", () => {
+        if (response.statusCode > 299) {
+          return reject(new Error(response.statusMessage));
+        }
+        const result: T = JSON.parse(data);
+        const resultObj = {
           success: true,
-          data: result
+          data: result,
         };
-        return res(resultObj);
+        return resolve(resultObj);
       });
     });
-  
+
     // Logging errors if present in the API request
-    request.on('error', (error) => {
-      console.error(error);
-      let errorObj = {
+    request.on("error", (error) => {
+      const errorObj = {
         error: true,
         errorMessage: error,
-      }
-      return rej(errorObj);
+      };
+      return reject(errorObj);
     });
-  
+
     // End the request
     request.end();
-
   });
-  return finalResponse;
-}
+};
 
-const getCharacter = async (characterUrl: string) => {
-  return await hitExternalRequest(characterUrl);
-}
+/**
+ * Fetch character detailss
+ *
+ * @param {string} characterUrl
+ * @returns {Promise<EpisodeCharacter>}
+ */
+const getEpisodeCharacter = async (
+  characterUrl: string
+): Promise<EpisodeCharacter | string> => {
+  try {
+    const { data, success, error } =
+      await makeExternalRequest<EpisodeCharacter>({
+        ...characterEndpointHttpRequestOptions,
+        path: characterUrl,
+      });
 
-const getEpisodes = async () => {
-  let episodeUrl = "https://rickandmortyapi.com/api/episode";
-  let response:any = await hitExternalRequest(episodeUrl);
-  let results: any = response?.data?.results;
-  if(results?.length > 0) {
-    const finalResults = Promise.all(
-      results.map(async (episode) => {
-        const characters = episode?.characters;
-        const modifiedCharacters = await Promise.all(characters.map(async (character, indexCharacter) => {
-          let rt:any = await getCharacter(character);
-          return rt?.data;
-        }));
-        return {
-          ...episode, characters: modifiedCharacters
-        }
-      })
-    ).then((finalResponse) => {
-      console.log("Final")
-      console.log(finalResponse);
-    });
+    return data as EpisodeCharacter; // Typecast Added for readability
+  } catch (error) {
+    //If error then just return the original url as fallback
+    if (error) {
+      return characterUrl;
+    }
   }
 };
 
-getEpisodes();
+/**
+ * Fetch list of the Episodes
+ *
+ * @returns {Promise<Episode[]>}
+ */
+const fetchEpisode = async (): Promise<Episode[]> => {
+  const { data } = await makeExternalRequest<EpisodeHttpReponse>(
+    episodeEndpointHttpRequestOptions
+  );
+
+  return data.results;
+};
+
+/**
+ * Fetches episodes list anf async fetch each episode character details
+ * @returns
+ */
+const main = async () => {
+  const episodes: Episode[] = await fetchEpisode();
+
+  // Notice use of await because of "try catch"
+  return Promise.all(
+    episodes.map(async (episode) => {
+      const characterUrls = episode.characters;
+      // Asynchronously fetch character details using url
+      const characterDetails = await Promise.all(
+        characterUrls.map(
+          async (characterUrl) => await getEpisodeCharacter(characterUrl)
+        )
+      );
+
+      return {
+        ...episode,
+        characters: characterDetails,
+      };
+    })
+  );
+};
+
+/** Main Function to start the Node app and execute the code to fetch the list of Episodes */
+main()
+  .then((response) => console.log(response))
+  .catch((error) => {
+    console.log(`Something bad happened: ${error.stack}`);
+  });
+
+/********** END OF CODE *******/
